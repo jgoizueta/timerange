a72818f472c16493dcfa8ad0c06afb4126ac79c2
Javier Goizueta <jgoizueta@gmail.com>
2019-05-15 15:31:36 +0200

    Code documentation (WIP)

--- timerange.js
+++ timerange.js
@@ -1,19 +1,3 @@
-/**
- *   A TimeRange consists of a time interval (start, end times, representing t such that start <= t < end)
- *   and a resolution.
- *   This can be represented uniquely as a text (abbreviated form),
- *   and has an implicit duration (in the resolution units).
- *   The interval can be represented in ISO format, but it doesn't determine
- *   the time range, since the same interval can be interpreted at different resolutions
- *   (e.g. 1 year, 365 days, ...). The interval (start, end times) need to
- *   be consistent with the resolution units (lie at unit boundaries).
- *   A TimeRange doesn't keep track of the time zone; all methods that involve multiple time
- *   ranges or time instants assume they all share the same reference time zone.
- *   The ordinal time values used here are UNIX-like epoch values, but we assume them to correspond to the reference time zone,
- *   not necessarily UTC.
- */
-
-
 const {
     timeStartEndToText,
     textToTimeStartEnd,
@@ -29,6 +13,42 @@ function startEndTimeValues (value, attr) {
     return [dateValue(...start), dateValue(...end), { resolution, [attr]: value }];
 }
 
+/**
+ * Class TimeRange represents an interval of time between to instants
+ * It includes its start instant and excludes the end instant: start <= t < end.
+ *
+ * It also includes an underlying resolution, so that the same time span,
+ * e.g. year 2018, can be represented as a 1-year or a 12-month time range, etc.
+ *
+ * Besides the standardized (ISO 8601) representation, e.g. 2018/2019 for year 2018,
+ * (or more precisely, 2018-01-01T00:00:00/2019-01-01T00:00:00),
+ * an abbreviated text form is supported, where 2018 would represent said interval,
+ * and 2018-01..2018-12 would represent it with month resolution. Note that whereas
+ * the ISO interval format excludes the final period, the abbreviated form is inclusive.
+ *
+ * Time ranges can hold single or multiple unit-of-time intervals such as a calender year,
+ * month, day, hour, etc., as well as arbitrary intervals. Minimum resolution is in seconds.
+ *
+ * The interval start and end instants need to be consistent with the resolution units
+ * (lie at unit boundaries).
+ *
+ * A TimeRange doesn't keep track of the time zone; all methods that involve multiple time
+ * ranges or time instants assume they all share the same reference time zone.
+ * The ordinal time values used here are UNIX-like epoch values, but we assume them to
+ * correspond to the reference time zone, not necessarily UTC.
+ *
+ * @param {String} text - abbreviated text representation of the range
+ * @param {String} iso - ISO 8601 form
+ * @param {Number} startValue - start of the range as elapsed milliseconds since a timezone-specific epoch
+ * @param {Number} endValue - end of the range as elapsed milliseconds since a timezone-specific epoch
+ * @param {TimeInstant} start - structured form of the start of the range
+ * @param {TimeInstant} end - structured form of the end of the range
+ * @param {Number} duration - length of the period in the units of resolution
+ * @param {String} resolution - units of resolution
+ *
+ * @name TimeRange
+ * @api
+ * */
 module.exports = class TimeRange {
     constructor (startValue, endValue, { text=null, iso=null, duration=null, resolution=null }) {
         this._text = text;
@@ -39,40 +59,115 @@ module.exports = class TimeRange {
         this._duration = duration;
     }
 
+    /**
+     * Construct a TimeRange from an time range string
+     *
+     * @param {String} text - Abbreviated ISO-formatted string (e.g. `'2018-03'`)
+     * @return {TimeRange}
+     * @api
+     */
     static fromText (text) {
         return new TimeRange(...startEndTimeValues(text, 'text'));
     }
 
+    /**
+     * Construct a TimeRange from an ISO 8601 interval string
+     *
+     * @param {String} iso - Interval string (e.g. `'2018-03/04'`)
+     * @return {TimeRange}
+     * @api
+     */
     static fromISO (iso) {
         return new TimeRange(...startEndTimeValues(iso, 'iso'));
     }
 
+    /**
+     * Construct a TimeRange from start and end epoch values in milliseconds.
+     *
+     * @param {Number} startValue - start of the range as elapsed milliseconds since a timezone-specific epoch
+     * @param {Number} endValue - end of the range as elapsed milliseconds since a timezone-specific epoch
+     * @return {TimeRange}
+     * @api
+     */
     static fromStartEndValues (startValue, endValue, resolution=null) {
         return new TimeRange(startValue, endValue, { resolution });
     }
 
+    /**
+     * Construct a TimeRange from start and end time instants
+     *
+     * @param {TimeInstant} start - start of the range
+     * @param {TimeInstant} end - end of the range
+     * @return {TimeRange}
+     * @api
+     */
     static fromStartEnd (start, end, resolution=null) {
         return TimeRange.fromStartEndValues(start.value, end.value, resolution);
     }
 
+    /**
+     * Construct a TimeRange from start value in milliseconds and duration
+     *
+     * @param {Number} startValue - start of the range as elapsed milliseconds since a timezone-specific epoch
+     * @param {Number} duration - number of units of resolution in the period
+     * @param {String} resolution - units of resolution ('millennium', 'century', 'year', ...)
+     * @param {String} adjust - optional rounding mode ('floor', 'ceil') applied to start value
+     * @return {TimeRange}
+     * @api
+     */
     static fromStartValueDuration (startValue, duration, resolution, adjust='floor') {
         startValue = roundDateValue(startValue, resolution, adjust);
         const endValue = incDateValue(startValue, resolution, duration);
         return TimeRange.fromStartEndValues(startValue, endValue, resolution);
     }
 
+    /**
+     * Construct a TimeRange from start and duration
+     *
+     * @param {TimeInstant} start - start of the range
+     * @param {Number} duration - number of units of resolution in the period
+     * @param {String} resolution - units of resolution ('millennium', 'century', 'year', ...)
+     * @param {String} adjust - optional rounding mode ('floor', 'ceil') applied to start value
+     * @return {TimeRange}
+     * @api
+     */
     static fromStartDuration (start, duration, resolution, adjust='floor') {
         return TimeRange.fromStartValueDuration(start.value, duration, resolution, adjust);
     }
 
+    /**
+     * Convert a time range to a different resolution, e.g.
+     * TimeRangel.fromText('2018').in('month').text // => '2018-01..2018-12'
+     *
+     * @param {String} resolutionUnits - new resolution units ('millennium', 'century', ...)
+     * @return {TimeRange}
+     * @api
+     */
     in(resolutionUnits) {
         return TimeRange.fromStartEndValues(this._startValue, this._endValue, resolutionUnits);
     }
 
+    /**
+     * Duration of a time range in seconds
+     *
+     * @return {Number}
+     * @api
+     */
     get durationSeconds () {
         return (this._endValue - this._startValue)/1000;
     }
 
+    /**
+     * Abbreviated text representation of a time range. For simple calendar units
+     * a simple form such as '2018-03' (month), '2018-03-01T11' (hour) is used,
+     * and for compound periods two such representations are joined with a '..',
+     * e.g. '2018-03..2018-04' or '2018-03..04' (a two-month period). Note that,
+     * unlike the ISO interval format or the start/end properties, this is includes
+     * the full final period (after the two dots).
+     *
+     * @return {String}
+     * @api
+     */
     get text () {
         if (this._text === null) {
             this._set();
@@ -80,6 +175,25 @@ module.exports = class TimeRange {
         return this._text;
     }
 
+    /**
+     * ISO 8601 text representation as a time interval.
+     *
+     * @return {String}
+     * @api
+     */
+    get iso () {
+        if (this._iso === null) {
+            this._set();
+        }
+        return this._iso;
+    }
+
+    /**
+     * Check if a time range is null (zero duration)
+     *
+     * @return {Boolean}
+     * @api
+     */
     get isEmpty () {
         return this._startValue >= this._endValue;
     }
@@ -96,13 +210,6 @@ module.exports = class TimeRange {
             this._duration = duration;
             this._resolution = resolution;
         }
-    }
-
-    get iso () {
-        if (this._iso === null) {
-            this._set();
-        }
-        return this._iso;
     }
 
     get startValue () {
