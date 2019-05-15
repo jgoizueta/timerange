521d38f9c3f65f74cd83a93688d923631054d634
Javier Goizueta <jgoizueta@gmail.com>
2019-05-15 12:53:08 +0200

    Remove time zone information

--- timeinstant.js
+++ timeinstant.js
@@ -1,34 +1,29 @@
 const { msToDate, dateValue, valueComponents } = require('./time');
 const { timeStartEndToText } = require('./conversions');
 
-// Fixed-time-zone dates.
+// Time zone-less dates.
 // This represent a time instant (point in time) as a
 // epoch milliseconds value or as structured fields (up to second precision).
-// It includes an optional informational time zone string that declares
-// to what TZ this value is related.
+// in reference to some unspecified time zone.
 // The idea is that this represent a date, just like a Date object,
 // but in relation ot some externally defined time zone
 // so we want to avoid the time zone handling that Date does.
 // These dates cannot be converted from the TZ they're specified in,
 // not even to/from UTC.
 module.exports = class TimeInstant {
-    constructor (milliseconds, tz = null) {
+    constructor (milliseconds) {
         this._value = milliseconds;
         this._date = msToDate(milliseconds);
-        this._timeZone = tz; // informational
     }
-    static fromValue (milliseconds, tz = null) {
-        return new TimeInstant(milliseconds, tz);
+    static fromValue (milliseconds) {
+        return new TimeInstant(milliseconds);
     }
-    static fromComponents (components, tz = null) {
-        return this.fromValue(dateValue(...components), tz);
+    static fromComponents (components) {
+        return this.fromValue(dateValue(...components));
     }
     // static fromUTCDate (date) {
     //     return this.fromValue(date.getTime());
     // }
-    static fromTZ (tz, ...components) {
-        return this.fromComponents(components, tz);
-    }
 
     get value () {
         return this._value;
@@ -58,8 +53,5 @@ module.exports = class TimeInstant {
     }
     get text () {
         return timeStartEndToText(this._value, this._value + 1000).iso;
-    }
-    get timeZone () {
-        return this._timeZone;
     }
 }
