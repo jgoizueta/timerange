060b64c56ac8b4f7b4d4f0e540d15985768371db
Javier Goizueta <jgoizueta@gmail.com>
2019-05-21 22:52:19 +0200

    TimeInstant rounding

--- test/timeinstant.test.js
+++ test/timeinstant.test.js
@@ -50,4 +50,87 @@ describe('TimeInstant', () => {
         expect(t.minute).toEqual(c[4]);
         expect(t.second).toEqual(c[5]);
      });
+
+     test('round down', () => {
+         expect(
+             TimeInstant.fromText('2018-04-12T13:34:42').round('second').text
+         ).toEqual('2018-04-12T13:34:42');
+         expect(
+            TimeInstant.fromText('2018-04-12T13:34:42').round('minute').text
+        ).toEqual('2018-04-12T13:34:00');
+        expect(
+            TimeInstant.fromText('2018-04-12T13:34:42').round('hour').text
+        ).toEqual('2018-04-12T13:00:00');
+        expect(
+            TimeInstant.fromText('2018-04-12T13:34:42').round('day').text
+        ).toEqual('2018-04-12T00:00:00');
+        expect(
+            TimeInstant.fromText('2018-04-12T13:34:42').round('month').text
+        ).toEqual('2018-04-01T00:00:00');
+        expect(
+            TimeInstant.fromText('2018-04-12T13:34:42').round('year').text
+        ).toEqual('2018-01-01T00:00:00');
+        expect(
+            TimeInstant.fromText('2018-04-12T13:34:42').round('week').text
+        ).toEqual('2018-04-09T00:00:00');
+        expect(
+            TimeInstant.fromText('2018-04-12T13:34:42').round('quarter').text
+        ).toEqual('2018-04-01T00:00:00');
+        expect(
+            TimeInstant.fromText('2018-04-12T13:34:42').round('trimester').text
+        ).toEqual('2018-01-01T00:00:00');
+        expect(
+            TimeInstant.fromText('2018-04-12T13:34:42').round('semester').text
+        ).toEqual('2018-01-01T00:00:00');
+        expect(
+            TimeInstant.fromText('2018-04-12T13:34:42').round('decade').text
+        ).toEqual('2011-01-01T00:00:00');
+        expect(
+            TimeInstant.fromText('2018-04-12T13:34:42').round('century').text
+        ).toEqual('2001-01-01T00:00:00');
+        expect(
+            TimeInstant.fromText('2018-04-12T13:34:42').round('millennium').text
+        ).toEqual('2001-01-01T00:00:00');
+    });
+    test('round up', () => {
+        expect(
+            TimeInstant.fromText('2018-04-12T13:34:42').round('second', 'ceil').text
+        ).toEqual('2018-04-12T13:34:42');
+        expect(
+           TimeInstant.fromText('2018-04-12T13:34:42').round('minute', 'ceil').text
+       ).toEqual('2018-04-12T13:35:00');
+       expect(
+           TimeInstant.fromText('2018-04-12T13:34:42').round('hour', 'ceil').text
+       ).toEqual('2018-04-12T14:00:00');
+       expect(
+           TimeInstant.fromText('2018-04-12T13:34:42').round('day', 'ceil').text
+       ).toEqual('2018-04-13T00:00:00');
+       expect(
+           TimeInstant.fromText('2018-04-12T13:34:42').round('month', 'ceil').text
+       ).toEqual('2018-05-01T00:00:00');
+       expect(
+           TimeInstant.fromText('2018-04-12T13:34:42').round('year', 'ceil').text
+       ).toEqual('2019-01-01T00:00:00');
+       expect(
+           TimeInstant.fromText('2018-04-12T13:34:42').round('week', 'ceil').text
+       ).toEqual('2018-04-16T00:00:00');
+       expect(
+           TimeInstant.fromText('2018-04-12T13:34:42').round('quarter', 'ceil').text
+       ).toEqual('2018-07-01T00:00:00');
+       expect(
+           TimeInstant.fromText('2018-04-12T13:34:42').round('trimester', 'ceil').text
+       ).toEqual('2018-05-01T00:00:00');
+       expect(
+           TimeInstant.fromText('2018-04-12T13:34:42').round('semester', 'ceil').text
+       ).toEqual('2018-07-01T00:00:00');
+       expect(
+           TimeInstant.fromText('2018-04-12T13:34:42').round('decade', 'ceil').text
+       ).toEqual('2021-01-01T00:00:00');
+       expect(
+           TimeInstant.fromText('2018-04-12T13:34:42').round('century', 'ceil').text
+       ).toEqual('2101-01-01T00:00:00');
+       expect(
+           TimeInstant.fromText('2018-04-12T13:34:42').round('millennium', 'ceil').text
+       ).toEqual('3001-01-01T00:00:00');
+   });
  });
